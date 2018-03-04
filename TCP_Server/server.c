e3f218733b1ef2568dec61e845de741125d94b83
momonarie22 <momonarie22@gmail.com>
2018-03-04 18:59:32 -0500

    initial commit

---
+++ TCP_Server/server.c
@@ -0,0 +1,24 @@
+/*
+ * Name:        TCP Chat Server
+ * Author:      Moises Beato Nunez
+ * Date:        04 Mar 2018
+ * Description: My attemp at a TCP Chat Server meant to help me practice my C system programming. As I get better, 
+ *              so does the chat server. I'm using various sources and tutorials to get me started
+ * 
+ * sources:     Learning - X YouTube channel -> https://www.youtube.com/channel/UCYtlE9Ws_5LsxTM17sK5Qnw 
+ *              GeeksfoGeeks article -> https://www.geeksforgeeks.org/socket-programming-cc/
+ *              Rensselaer School of Science article -> http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/type.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
