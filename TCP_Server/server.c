fa7524890f744288ffc3daa2f7acbff7bb57f204
Moises Beato Nunez <moises.beatonunez@gmail.com>
2018-03-18 17:57:54 -0400

    minor update
    *not the server prints the name and ip information of the incomming connection

--- TCP_Server/server.c
+++ TCP_Server/server.c
@@ -39,61 +39,63 @@ int main(int argc, char **argv)
 {
 
 
-    char *message = "Welcome. You have reached the server. Type --q to quit.";  //welcome message
+    char *message = "Welcome. You have reached the server. Ctrl-C to quit.";  //welcome message
     char buffer[255];                //holds the messages from client
-    char client_name[15];            //hold the name of the client  
-    
+    char client_name[15];            //hold the name of the client
+    char remoteIP[INET6_ADDRSTRLEN];
+
     int opt = 1;                     //option to make the server address reusable
     int server_socket, new_socket, client_socket[MAX_CLIENTS]; //listening socket, incomming socket and a array of the connected sockets
     int activity;                    //used with select() to check for incomming connection or messages
     int msgval;                      //hold the number of chars of the incomming massave
     int nameval;                     //hold the value of sender's name
-    int i;          
+    int i;
     int sd, max_sd;                  //holds socket descriptors
-    
+
     struct addrinfo address, *res, *p; //server address information
     struct sockaddr_storage clnt_addr; //connection address information
     socklen_t addr_size;               //size of the connection address
-   
+
     //set of socket descriptors
     fd_set readfds;
-    
+
     //check main arguments
     if (argc < 2)
-      fprintf(stderr, "[SYS_MSG]: Port Not Provided.\n"); exit(1);
-    
-    int port_num = argv[1];  //port number to listen on
+      fprintf(stderr, "[SYS_MSG]: Port Not Provided.\n");
+
+
 
     puts("[SYS_MSG]: Initializing......");
 
     memset(&address,0 ,sizeof(address)); //clear the address
-  
+
     //specify an address for the socket
     address.ai_family = AF_INET;            //use IPV4
     address.ai_socktype = SOCK_STREAM;      //use a TCP connection
     address.ai_flags = AI_PASSIVE;          //use the IP address of the machine running the program
-    
+
     //get the ip address info.
-    int status = getaddrinfo(NULL, port_num , &address, &res); 
-    if (status < 0) 
+    int status = getaddrinfo(NULL,argv[1] , &address, &res);
+    if (status < 0)
         error("[SYS_MSG]: getaddrinfo fail.");
-        
+
+    int port_num = atoi(argv[1]);  //port number to listen on
     //initialize all client_socket[] to 0
      memset(client_socket,0 ,sizeof(client_socket));
-    
+
     // loop through all the results and bind to the first we can
     for(p = res; p != NULL; p = p->ai_next) {
         //creating a socket
-        if ((server_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+        if ((server_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
             perror("[SYS_MSG]: socket creation failed.");
             continue;
         }
         //set server socket to allow multiple connections
         if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
             error("[SYS_MSG]: setsockopt Failed.");
-        
+
         //bind socket to address
-        if (bind(server_socket, p->ai_addr, p->ai_addrlen) == -1) {
+        if (bind(server_socket, p->ai_addr, p->ai_addrlen) < 0) {
             close(server_socket);
             perror("[SYS_MSG]: bind");
             continue;
@@ -102,23 +104,23 @@ int main(int argc, char **argv)
     }
     // all done with this structure
     freeaddrinfo(res);
-    
+
     //if bind fails
-    if (p == NULL) 
+    if (p == NULL)
     error("[SYS_MSG]: Failed to Bind.");
- 
+
     printf("[SYS_MSG]: Listening on port %d......\n", port_num);
-    
+
     //listen for connection
-    if(listen(server_socket, MAX_CLIENTS) < 0) 
+    if(listen(server_socket, MAX_CLIENTS) < 0)
         error("[SYS_MSG]: Error Listning");
 
     //accept the incoming connections
     puts("[SYS_MSG]: Waiting for connection......");
-    
+
     //set the size of the incomming address
     addr_size = sizeof clnt_addr;
-    
+
     while (1){  //do forever
         //clear the file descriptor socket set
         FD_ZERO(&readfds);
@@ -149,10 +151,10 @@ int main(int argc, char **argv)
             new_socket = accept(server_socket, (struct sockaddr *)&clnt_addr, &addr_size);
             if (new_socket < 0) error("[SYS_MSG]: Incomming Connection Not Accepted."); //check for errors
             //print the new connection's data
-            // printf("[SYS_MSG]: New Connection.\n"
-            // "\tSocket FD  -> %d\n"
-            // "\tIP Address -> %s\n"
-            // "\tPort       -> %d\n", new_socket, inet_ntop(clnt_addr), port_num);
+            printf("[SYS_MSG]: New Connection.\n"
+            "\tSocket FD  -> %d\n"
+            "\tIP Address -> %s\n"
+            "\tPort       -> %d\n", new_socket, inet_ntop(clnt_addr.ss_family, (struct sockaddr*)&clnt_addr, remoteIP, INET6_ADDRSTRLEN), port_num);
             //send the new connection a greeting
             if (send(new_socket, message, strlen(message), 0) != strlen(message))
                 error("[SYS_MSG]: Send failed.");  //check for errors
@@ -176,40 +178,39 @@ int main(int argc, char **argv)
             sd = client_socket[i];
             //check for activity in the socket
             if (FD_ISSET(sd, &readfds)){
-                nameval = recv(sd, client_name, sizeof(client_name), 0);          //read name of sender
-                msgval = recv(sd, buffer, sizeof(buffer), 0);                   //read incomming message
+                nameval = recv(sd, client_name, sizeof(client_name)-1, 0);          //read name of sender
+                msgval = recv(sd, buffer, sizeof(buffer)-1, 0);                   //read incomming message
+                client_name[nameval] = '\0';
+                buffer[msgval] = '\0';                           //add null terminator to the end of the recieved messege
 
-                if (msgval == 0){                           //check for a disconnection
+                printf("[%s]: %s\n", client_name, buffer);    //print message to the screen
+
+                if (msgval == 0 || nameval == 0){                           //check for a disconnection
                     getpeername(sd, (struct sockaddr *)&clnt_addr, &addr_size); //get socket data
 
-                    // printf("[SYS_MSG]:Client Disconnected.\n"               //print a diconnect message
-                    // "\tName       -> %s\n"
-                    // "\tIP Address -> %s\n"
-                    // "\tPort       -> %d\n",client_name, inet_ntop(clnt_addr), port_num);
-                    
+                    printf("[SYS_MSG]:Client Disconnected.\n"               //print a diconnect message
+                    "\tName       -> %s\n"
+                    "\tIP Address -> %s\n"
+                    "\tPort       -> %d\n",client_name, inet_ntop(clnt_addr.ss_family, (struct sockaddr*)&clnt_addr, remoteIP, INET6_ADDRSTRLEN), port_num);
+
                     FD_CLR(sd, &readfds); // remove from master set
                     close(sd); //close the socket
-                    client_socket[i] = 0;   
+                    client_socket[i] = 0;
 
                 } else {
-                    client_name[nameval] = '\0';
-                    buffer[msgval] = '\0';                           //add null terminator to the end of the recieved messege
-
-                    printf("[%s]: %s\n", client_name, buffer);    //print message to the screen
-
                     //echo message back to all clients, including the name of the message sender
                     for (int j = 0; j < MAX_CLIENTS; j++){
-                        if (client_socket[j] == 0) continue; //skip empty or closed sockets
-                        else{
-                            sd = client_socket[j];
-                            send(sd, client_name, strlen(client_name), 0);
-                            send(sd, buffer, strlen(buffer), 0);
+                        if (FD_ISSET(client_socket[j], &readfds))
+                        {   if (client_socket[j] != server_socket){
+                              if (send(client_socket[j], client_name, nameval, 0) == -1) perror("Send Name");
+                              if (send(client_socket[j], buffer, msgval, 0) == -1) perror("Send Msg");
+                            }
                         }
                     }
                 }
             }
         }
     }
-   
-    return 0;    
+
+    return 0;
 }
